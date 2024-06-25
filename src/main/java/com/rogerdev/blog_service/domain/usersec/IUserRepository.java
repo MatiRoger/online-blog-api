690eb08d7f70d5ec6731db8421b314221a4dcdc9
Matias Roger Narcotti <matiasrogern.dev@gmail.com>
2024-06-25 18:55:50 -0300

    feat: created custom repository method

--- src/main/java/com/rogerdev/blog_service/domain/usersec/IUserRepository.java
+++ src/main/java/com/rogerdev/blog_service/domain/usersec/IUserRepository.java
@@ -3,6 +3,9 @@ package com.rogerdev.blog_service.domain.usersec;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface IUserRepository extends JpaRepository<UserSec, Long> {
+    Optional<UserSec> getUserSecEntityByUsername(String username);
 }
