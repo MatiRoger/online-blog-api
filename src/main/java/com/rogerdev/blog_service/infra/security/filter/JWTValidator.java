061b56cf073ddf85f380c525bf4d34d1e3577678
Matias Roger Narcotti <matiasrogern.dev@gmail.com>
2024-06-28 09:59:07 -0300

    feat: created jwt validation filter

--- src/main/java/com/rogerdev/blog_service/infra/security/filter/JWTValidator.java
+++ src/main/java/com/rogerdev/blog_service/infra/security/filter/JWTValidator.java
@@ -1,19 +1,51 @@
 package com.rogerdev.blog_service.infra.security.filter;
 
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.rogerdev.blog_service.infra.utils.JwtUtils;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpHeaders;
+import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Collection;
 
 public class JWTValidator extends OncePerRequestFilter {
+    private JwtUtils jwtUtils;
+    public JWTValidator(JwtUtils jwtUtils) {
+    }
+
     @Override
     protected void doFilterInternal(
-            HttpServletRequest request,
-            HttpServletResponse response,
-            FilterChain filterChain) throws ServletException, IOException {
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain) throws ServletException, IOException {
 
+        String token = request.getHeader(HttpHeaders.AUTHORIZATION);
+
+        if (token != null) {
+            DecodedJWT decodedJWT = jwtUtils.validateToken(token);
+
+            String username = jwtUtils.extractUsername(decodedJWT);
+            String authorities = jwtUtils.extractSpecificClaim(decodedJWT, "authorities").asString();
+
+            Collection<? extends GrantedAuthority> authoritiesList = AuthorityUtils.commaSeparatedStringToAuthorityList(authorities);
+
+            Authentication auth = new UsernamePasswordAuthenticationToken(username, null, authoritiesList);
+            SecurityContext context = SecurityContextHolder.getContext();
+
+            context.setAuthentication(auth);
+            SecurityContextHolder.setContext(context);
+        }
+        filterChain.doFilter(request, response);
     }
 }
